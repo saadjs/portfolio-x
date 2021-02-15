077b0ba01ff9c8bbb5fa75ef61ab045902bc649b
Saad <saad@saadshaikh.dev>
2021-02-15 13:43:06 -0500

    complete form and design slack bot on submission

--- src/components/theme/Header/Navbar/index.jsx
+++ src/components/theme/Header/Navbar/index.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <Wrapper as={Container}>
       <Brand as={Link} to="/" theme={theme}>
-        John Doe
+        Saad Shaikh
       </Brand>
       <NavbarLinks desktop />
     </Wrapper>
--- src/data/config.js
+++ src/data/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  defaultTitle: 'John Doe',
+  defaultTitle: 'Saad Shaikh',
   logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
-  author: 'John Doe',
+  author: 'Saad Shaikh',
   url: 'https://portfolio.smakosh.com',
-  legalName: 'John Doe',
-  defaultDescription: 'I’m John and I’m a Backend & Devops engineer!',
+  legalName: 'Saad Shaikh',
+  defaultDescription: 'I’m Saad and I’m a Backend & Devops engineer!',
   socialLinks: {
     twitter: 'http://www.twitter.com/smakosh',
     github: 'https://github.com/smakosh',
