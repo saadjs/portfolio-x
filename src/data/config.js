b69bd02cb16c70f57c3483264bb449e4ee6d2985
Saad <saad@saadshaikh.dev>
2021-02-15 17:50:45 -0500

    cleanup

--- src/data/config.js
+++ src/data/config.js
@@ -2,7 +2,7 @@ module.exports = {
   defaultTitle: 'Saad Shaikh',
   logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
   author: 'Saad Shaikh',
-  url: 'https://portfolio.smakosh.com',
+  url: 'https://saadshaikh.dev',
   legalName: 'Saad Shaikh',
   defaultDescription: 'I’m Saad and I’m a Backend & Devops engineer!',
   socialLinks: {
@@ -14,7 +14,7 @@ module.exports = {
   themeColor: '#6b63ff',
   backgroundColor: '#6b63ff',
   social: {
-    facebook: 'appId',
+    facebook: '',
     twitter: '@notThatSaad',
   },
   address: {
